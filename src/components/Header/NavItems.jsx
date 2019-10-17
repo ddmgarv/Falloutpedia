8fbd2da8d9229494097080787194fb6ae9902c9f
Gustavo <garv1998131@gmail.com>
2019-10-16 21:59:07 -0300

    new branch

--- src/components/Header/NavItems.jsx
+++ src/components/Header/NavItems.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import { falloutLogo } from "../../constants/imgs";
 
-const NavItems = ({ options, games }) => (
-  <ul className="nav__list">
-    <li>
-      <img src={falloutLogo} alt="" className="img" />
-    </li>
-    {options.map(option => (
-      <li className="nav__list--item">{option}</li>
-    ))}
-  </ul>
-);
+const NavItems = ({ options, games }) => {
+  const insertGames = () => (
+    <div className="">
+      <ul>
+        {games.map(game => (
+          <li className="">{game}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  return (
+    <ul className="nav__list">
+      <li>
+        <img src={falloutLogo} alt="" className="img" />
+      </li>
+      {options.map(option =>
+        option === "juegos" ? (
+          <li className="nav__list--item">
+            {option}
+            {insertGames()}
+          </li>
+        ) : (
+          <li className="nav__list--item">{option}</li>
+        )
+      )}
+    </ul>
+  );
+};
 
 export default NavItems;
