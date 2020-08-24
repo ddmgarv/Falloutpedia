85ac5518323330b815d2d50201e0b43dd2bcb500
Gustavo <garv1998131@gmail.com>
2020-08-24 20:30:03 -0300

    added firebase, refactored power armor section

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from "react";
-import Nav from "./components/Nav";
-import Home from "./components/Home";
-import PowerArmor from "./components/PowerArmor";
-import { BrowserRouter, Route } from "react-router-dom";
-import "./App.scss";
+import React from 'react';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import PowerArmor from './components/PowerArmor';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.scss';
 
 function App() {
-  return (
-    <main className="app">
-      <BrowserRouter>
-        <Nav />
-        <Route path={"/inicio"} component={Home} />
-        <Route path={"/power%20armor"} component={PowerArmor} />
-      </BrowserRouter>
-    </main>
-  );
+	return (
+		<main className='app'>
+			<BrowserRouter>
+				<Nav />
+				<Route path={'/inicio'} component={Home} />
+				<Route path={'/'} component={PowerArmor} />
+			</BrowserRouter>
+		</main>
+	);
 }
 
 export default App;
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,67 +1,53 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import * as optionsActions from "../../actions/optionsActions";
-import NavItems from "./NavItems";
-import Loader from "../Utilities/Loader";
-import "./nav.scss";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as optionsActions from '../../actions/optionsActions';
+import NavItems from './NavItems';
+import Loader from '../Utilities/Loader';
+import './nav.scss';
 
 class Nav extends Component {
-  state = {
-    dropdown: false
-  };
+	state = {
+		dropdown: false,
+	};
 
-  componentDidMount() {
-    this.props.getOptions();
-  }
+	componentDidMount() {
+		this.props.getOptions();
+	}
 
-  handleDropdown = () => {
-    const {
-      state,
-      state: { dropdown }
-    } = this;
-    this.setState({
-      ...state,
-      dropdown: !dropdown
-    });
-  };
+	handleDropdown = () => {
+		const { dropdown } = this.state;
+		this.setState({ dropdown: !dropdown });
+	};
 
-  renderOptions() {
-    const {
-      loading,
-      error,
-      navData: { games, options },
-      navData
-    } = this.props;
-    const { dropdown } = this.state;
-
-    if (loading) {
-      return (
-        <div className="centered-container-full">
-          <Loader color={"#7d5a0d"} />
-        </div>
-      );
-    } else if (Object.keys(navData).length > 0) {
-      return (
-        <NavItems
-          games={games}
-          options={options}
-          dropdown={dropdown}
-          handleDropdown={this.handleDropdown}
-        />
-      );
-    } else if (error !== "") {
-      return "Error";
-    }
-  }
-  render() {
-    return (
-      <nav className="nav__wrapper">
-        <div className="nav__container">{this.renderOptions()}</div>
-      </nav>
-    );
-  }
+	renderOptions = () => {
+		const {
+			loading,
+			error,
+			navData: { games, options },
+			navData,
+		} = this.props;
+		const { dropdown } = this.state;
+		if (loading) {
+			return (
+				<div className='centered-container-full'>
+					<Loader color={'#7d5a0d'} />
+				</div>
+			);
+		} else if (Object.keys(navData).length > 0) {
+			return <NavItems games={games} options={options} dropdown={dropdown} handleDropdown={this.handleDropdown} />;
+		} else if (error !== '') {
+			return 'Error';
+		}
+	};
+	render() {
+		return (
+			<nav className='nav__wrapper'>
+				<div className='nav__container'>{this.renderOptions()}</div>
+			</nav>
+		);
+	}
 }
 
-const mapStateToProps = reducers => reducers.optionsReducer;
+const mapStateToProps = (state) => state.optionsReducer;
 
 export default connect(mapStateToProps, optionsActions)(Nav);
